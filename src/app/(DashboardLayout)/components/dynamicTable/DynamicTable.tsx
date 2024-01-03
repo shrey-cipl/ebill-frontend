b925245b70193100d565e3e373daf710d3471126
Shrey Sharma <shrey.sharma@cipl.org.in>
2024-01-03 15:35:45 +0530

    fix: remove and add button on mobile bill table

--- src/app/(DashboardLayout)/components/dynamicTable/DynamicTable.tsx
+++ src/app/(DashboardLayout)/components/dynamicTable/DynamicTable.tsx
@@ -1,39 +1,62 @@
-import React, { useState } from 'react';
-import { Button, Input, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import React, { useState } from "react"
+import {
+  Button,
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material"
 
 interface TableRowData {
-    phone: string;
-  periodFrom: string;
-  periodTo: string;
-  claimedAmount: string;
-  admissibleAmount: string;
+  phone: string
+  periodFrom: string
+  periodTo: string
+  claimedAmount: string
+  admissibleAmount: string
 }
 interface DynamicTableProps {
-    tableData: TableRowData[];
-    setTableData: React.Dispatch<React.SetStateAction<TableRowData[]>>;
+  tableData: TableRowData[]
+  setTableData: React.Dispatch<React.SetStateAction<TableRowData[]>>
+}
+
+const DynamicTable: React.FC<DynamicTableProps> = ({
+  tableData,
+  setTableData,
+}) => {
+  const addRow = () => {
+    setTableData([
+      ...tableData,
+      {
+        phone: "",
+        periodFrom: "",
+        periodTo: "",
+        claimedAmount: "",
+        admissibleAmount: "",
+      },
+    ])
   }
 
-const DynamicTable: React.FC<DynamicTableProps>  = ({ tableData, setTableData }) => {
-    
- 
-  const addRow = () => {
-    setTableData([...tableData, { phone: '', periodFrom: '', periodTo: '', claimedAmount: '', admissibleAmount: '' }]);
-  };
-
   const removeRow = (index: number) => {
-    const updatedData = [...tableData];
-    updatedData.splice(index, 1);
-    setTableData(updatedData);
-  };
+    const updatedData = [...tableData]
+    updatedData.splice(index, 1)
+    setTableData(updatedData)
+  }
 
-  const handleInputChange = (index: number, field: keyof TableRowData, value: string) => {
-    const updatedData = [...tableData];
+  const handleInputChange = (
+    index: number,
+    field: keyof TableRowData,
+    value: string
+  ) => {
+    const updatedData = [...tableData]
     updatedData[index] = {
       ...updatedData[index],
-      [field]: value
-    };
-    setTableData(updatedData);
-  };
+      [field]: value,
+    }
+    setTableData(updatedData)
+  }
 
   return (
     <div>
@@ -53,32 +76,75 @@ const DynamicTable: React.FC<DynamicTableProps>  = ({ tableData, setTableData })
             {tableData.map((row, index) => (
               <TableRow key={index}>
                 <TableCell>
-                  <Input type="text" value={row.phone} onChange={(e) => handleInputChange(index, 'phone', e.target.value)} />
+                  <Input
+                    type="text"
+                    value={row.phone}
+                    onChange={(e) =>
+                      handleInputChange(index, "phone", e.target.value)
+                    }
+                  />
                 </TableCell>
                 <TableCell>
-                  <Input type="date" value={row.periodFrom} onChange={(e) => handleInputChange(index, 'periodFrom', e.target.value)} />
+                  <Input
+                    type="date"
+                    value={row.periodFrom}
+                    onChange={(e) =>
+                      handleInputChange(index, "periodFrom", e.target.value)
+                    }
+                  />
                 </TableCell>
                 <TableCell>
-                  <Input type="date" value={row.periodTo} onChange={(e) => handleInputChange(index, 'periodTo', e.target.value)} />
+                  <Input
+                    type="date"
+                    value={row.periodTo}
+                    onChange={(e) =>
+                      handleInputChange(index, "periodTo", e.target.value)
+                    }
+                  />
                 </TableCell>
                 <TableCell>
-                  <Input type="text" value={row.claimedAmount} onChange={(e) => handleInputChange(index, 'claimedAmount', e.target.value)} />
+                  <Input
+                    type="text"
+                    value={row.claimedAmount}
+                    onChange={(e) =>
+                      handleInputChange(index, "claimedAmount", e.target.value)
+                    }
+                  />
                 </TableCell>
                 <TableCell>
-                  <Input type="text" value={row.admissibleAmount} onChange={(e) => handleInputChange(index, 'admissibleAmount', e.target.value)} />
+                  <Input
+                    type="text"
+                    value={row.admissibleAmount}
+                    onChange={(e) =>
+                      handleInputChange(
+                        index,
+                        "admissibleAmount",
+                        e.target.value
+                      )
+                    }
+                  />
                 </TableCell>
                 <TableCell>
-                  <Button onClick={index === tableData.length - 1 ? addRow : () => removeRow(index)} variant="contained" color={index === tableData.length - 1 ? "primary" : "secondary"}>
-                    {index === tableData.length - 1 ? "Add More" : "Remove"}
+                  <Button
+                    onClick={() => removeRow(index)}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    Remove
                   </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
+        <div style={{ width: "100%", textAlign: "center" }}>
+          <Button onClick={addRow} variant="contained" color={"primary"}>
+            Add More
+          </Button>
+        </div>
       </TableContainer>
     </div>
-  );
-};
+  )
+}
 
-export default DynamicTable;
+export default DynamicTable
