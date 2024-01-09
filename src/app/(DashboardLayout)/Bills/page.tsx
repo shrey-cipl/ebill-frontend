81080bf69f9d0709e471dac6539b36316afff4df
Shrey Sharma <shrey.sharma@cipl.org.in>
2024-01-09 10:58:32 +0530

    Refactor: implemented data-grid for bills page

--- src/app/(DashboardLayout)/Bills/page.tsx
+++ src/app/(DashboardLayout)/Bills/page.tsx
@@ -2,11 +2,6 @@
 import { useEffect, useState } from "react"
 
 import dayjs from "dayjs"
-import Table from "@mui/material/Table"
-import TableBody from "@mui/material/TableBody"
-import TableCell from "@mui/material/TableCell"
-import TableHead from "@mui/material/TableHead"
-import TableRow from "@mui/material/TableRow"
 import Button from "@mui/material/Button"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
@@ -25,22 +20,8 @@ import {
 import PageContainer from "../components/container/PageContainer"
 import DashboardNew from "../components/shared/DashboardNew"
 import CustomModal from "../components/CustomModal/CustomModal"
-import Pagination from "../components/Pagination/Pagination"
 import axiosApi from "@/Util/axiosApi"
 import { useAuth } from "@/context/JWTContext/AuthContext.provider"
-
-const BILLS_HEADERS = [
-  "S.No",
-  "Diary No.",
-  "Name",
-  "Type",
-  "Claimed Amount",
-  "Admissible Amount",
-  "Sanctioned Amount",
-  "Status",
-  "Last Updated",
-  "Action",
-]
 
 const BILL_MODES = { add: "add_bill", update: "update_bill" }
 
@@ -54,41 +35,18 @@ const BoxWrapper = styled("div")(() => ({
   },
 }))
 
-const OptionsWrapper = styled("div")(() => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-end",
-  gap: "10px",
-  marginBottom: "10px",
-}))
-
-const TabelCellStyled = styled(TableCell)(() => ({
-  fontSize: "12px",
-  padding: "10px 5px",
-  wordBreak: "normal",
-}))
-
-let tempCounter = 0
-
 const Bills = () => {
-  const [billList, setBillList] = useState<any>([])
-  const [filterInputs, setFilterInputs] = useState({
-    name: "",
-    diaryNumber: "",
-  })
-  const [pageNo, setPageNo] = useState(1)
-  const router = useRouter()
+  const [billList, setBillList] = useState([])
   // Modal states
   const [modalState, setModalState] = useState(false)
   const [selectedBill, setSelectedBill] = useState<any>({})
 
-  const [newData, setNewData] = useState([])
-
+  const router = useRouter()
   const authCtx: any = useAuth()
 
   const handleFetchBills = async () => {
     const config = {
-      url: `/api/claim/getall?limit=10&page=${pageNo}&name=${filterInputs.name}&diaryNumber=${filterInputs.diaryNumber}`,
+      url: `/api/claim/getall`,
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -98,6 +56,11 @@ const Bills = () => {
 
     try {
       const res = await axiosApi(config.url, config.method, config.headers)
+
+      for (let item of res.data) {
+        item.id = item._id
+      }
+
       setBillList(res.data)
       // if (String(res.status).charAt(0) === "2") {
       // }
@@ -106,67 +69,18 @@ const Bills = () => {
     }
   }
 
-  useEffect(() => {
-    const getData = async () => {
-      const config = {
-        url: `/api/claim/getall`,
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Bearer ${authCtx?.user?.token}`,
-        },
-      }
-
-      try {
-        const res: any = await axiosApi(
-          config.url,
-          config.method,
-          config.headers
-        )
-
-        for (let item of res.data) {
-          item.id = item._id
-        }
-
-        setNewData(res.data)
-        // if (String(res.status).charAt(0) === "2") {
-        // }
-      } catch (err: any) {
-        console.log(err.message)
-      }
-    }
-
-    getData()
-  }, [pageNo, tempCounter, authCtx?.user?.token])
-
   // collect bills and updates list
   useEffect(() => {
     handleFetchBills()
-  }, [pageNo, tempCounter, authCtx?.user?.token])
+  }, [authCtx?.user?.token])
 
   const handleViewBill = (id: any) => {
-    const filteredBill = newData.find((bill: any) => bill._id === id)
+    const filteredBill = billList.find((bill: any) => bill._id === id)
 
     if (filteredBill) {
       setSelectedBill(filteredBill)
       setModalState(true)
     }
-  }
-
-  const handleFilterInputs = (e: any) => {
-    setFilterInputs((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const handleClearFilters = () => {
-    setFilterInputs({
-      name: "",
-      diaryNumber: "",
-    })
-
-    tempCounter += 1
   }
 
   const columns: GridColDef[] = [
@@ -197,7 +111,6 @@ const Bills = () => {
     {
       field: "random_2",
       headerName: "Action",
-
       renderCell: (params) => {
         if (params.row.pendingBranch) {
           return (
@@ -236,9 +149,22 @@ const Bills = () => {
     <PageContainer title="Bills" description="List of all the bills">
       <DashboardNew title="Bills" titleVariant="h5">
         <>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              sx={{ background: "#9C27B0" }}
+              variant="contained"
+              size="small"
+              onClick={() =>
+                router.push(`/Bills/ManageBill?mode=${BILL_MODES.add}`)
+              }
+            >
+              Add New
+            </Button>
+          </div>
           <DataGrid
-            rows={newData}
+            rows={billList}
             columns={columns}
+            density="compact"
             sx={{
               ".bg-light": {
                 bgcolor: "#eee",
@@ -282,151 +208,6 @@ const Bills = () => {
             }}
             pageSizeOptions={[10, 25, 50, 100]}
           />
-          {/* <Box>
-            <OptionsWrapper>
-              <Button
-                sx={{ marginTop: "-23px" }}
-                variant="contained"
-                size="small"
-                onClick={() =>
-                  router.push(`/Bills/ManageBill?mode=${BILL_MODES.add}`)
-                }
-              >
-                Add New
-              </Button>
-              <Box sx={{ display: "flex", gap: "10px", alignItems: "center" }}>
-                <Typography>Search By:</Typography>
-                <TextField
-                  name="name"
-                  size="small"
-                  placeholder="Name"
-                  value={filterInputs.name}
-                  onChange={handleFilterInputs}
-                  sx={{ width: "125px" }}
-                />
-                <TextField
-                  name="diaryNumber"
-                  size="small"
-                  placeholder="Diary No."
-                  value={filterInputs.diaryNumber}
-                  onChange={handleFilterInputs}
-                  sx={{ width: "125px" }}
-                />
-                <Button
-                  variant="contained"
-                  size="small"
-                  sx={{ background: "#9C27B0" }}
-                  onClick={() => {
-                    setPageNo(1)
-                    handleFetchBills()
-                  }}
-                >
-                  Search
-                </Button>
-                <Button
-                  variant="contained"
-                  sx={{ background: "#9C27B0" }}
-                  size="small"
-                  onClick={handleClearFilters}
-                >
-                  Clear
-                </Button>
-              </Box>
-            </OptionsWrapper>
-          </Box>
-          <Box>
-            <Table
-              sx={{
-                display: "block",
-                overflowX: "auto",
-                minWidth: "500px",
-                width: "100%",
-              }}
-            >
-              <TableHead sx={{ width: "100%" }}>
-                <TableRow sx={{ background: "#4C7AFF", width: "100%" }}>
-                  {BILLS_HEADERS.map((header, i) => (
-                    <TableCell
-                      key={i}
-                      sx={{
-                        color: "white",
-                        padding: "5px",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {header}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {billList.map((bill: any, i: any) => {
-                  const rowColor = (i + 1) % 2 === 0 ? "#eee" : "#fff"
-
-                  const itemNumber = (pageNo - 1) * 10 + (i + 1)
-
-                  return (
-                    <TableRow key={bill._id} sx={{ background: rowColor }}>
-                      <TabelCellStyled>{`${itemNumber}.`}</TabelCellStyled>
-                      <TabelCellStyled>{bill.diaryNumber}</TabelCellStyled>
-                      <TabelCellStyled>{bill.former.name}</TabelCellStyled>
-                      <TabelCellStyled>{bill.billType}</TabelCellStyled>
-                      <TabelCellStyled>
-                        {bill.totalClaimedAmount}
-                      </TabelCellStyled>
-                      <TabelCellStyled>
-                        {bill.totalAdmissibleAmount}
-                      </TabelCellStyled>
-                      <TabelCellStyled>{bill.sanctionedAmount}</TabelCellStyled>
-                      <TabelCellStyled
-                        sx={{
-                          color:
-                            bill.currentStatus === "Open" ? "green" : "red",
-                        }}
-                      >
-                        {bill.currentStatus}
-                      </TabelCellStyled>
-                      <TabelCellStyled>
-                        {dayjs(bill.updatedAt).format("YYYY-MM-DD")}
-                      </TabelCellStyled>
-                      <TabelCellStyled>
-                        {bill.pendingBranch ? (
-                          <Link
-                            style={{
-                              color: "#4C7AFF",
-                              textDecoration: "none",
-                            }}
-                            href={`/Bills/ManageBill?bill_id=${bill._id}&mode=${BILL_MODES.update}`}
-                          >
-                            Update
-                          </Link>
-                        ) : (
-                          <button
-                            style={{
-                              background: "none",
-                              border: "none",
-                              color: "#4C7AFF",
-                              fontSize: "13px",
-                              padding: 0,
-                              cursor: "pointer",
-                            }}
-                            onClick={() => handleViewBill(bill._id)}
-                          >
-                            View
-                          </button>
-                        )}
-                      </TabelCellStyled>
-                    </TableRow>
-                  )
-                })}
-              </TableBody>
-            </Table>
-          </Box>
-          <Pagination
-            url={`/api/claim/getall?name=${filterInputs.name}&diaryNumber=${filterInputs.diaryNumber}`}
-            data={billList}
-            setPageNo={setPageNo}
-          /> */}
 
           {/* BILL MODAL */}
           <CustomModal modalState={modalState} setModalState={setModalState}>
