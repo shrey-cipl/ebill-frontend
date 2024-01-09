6afda02f4a4ee568f6f84b8c0a9b4dc1c35d11c9
Shrey Sharma <shrey.sharma@cipl.org.in>
2024-01-09 10:23:01 +0530

    Refactor: moved table to grid format

--- src/app/(DashboardLayout)/Bills/page.tsx
+++ src/app/(DashboardLayout)/Bills/page.tsx
@@ -15,6 +15,13 @@ import { styled } from "@mui/system"
 import { useRouter } from "next/navigation"
 import Link from "next/link"
 
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridToolbar,
+} from "@mui/x-data-grid"
+
 import PageContainer from "../components/container/PageContainer"
 import DashboardNew from "../components/shared/DashboardNew"
 import CustomModal from "../components/CustomModal/CustomModal"
@@ -75,6 +82,8 @@ const Bills = () => {
   const [modalState, setModalState] = useState(false)
   const [selectedBill, setSelectedBill] = useState<any>({})
 
+  const [newData, setNewData] = useState([])
+
   const authCtx: any = useAuth()
 
   const handleFetchBills = async () => {
@@ -89,7 +98,6 @@ const Bills = () => {
 
     try {
       const res = await axiosApi(config.url, config.method, config.headers)
-      // console.log(res,"ressssssssssssssssssssss");
       setBillList(res.data)
       // if (String(res.status).charAt(0) === "2") {
       // }
@@ -98,13 +106,46 @@ const Bills = () => {
     }
   }
 
+  useEffect(() => {
+    const getData = async () => {
+      const config = {
+        url: `/api/claim/getall`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${authCtx?.user?.token}`,
+        },
+      }
+
+      try {
+        const res: any = await axiosApi(
+          config.url,
+          config.method,
+          config.headers
+        )
+
+        for (let item of res.data) {
+          item.id = item._id
+        }
+
+        setNewData(res.data)
+        // if (String(res.status).charAt(0) === "2") {
+        // }
+      } catch (err: any) {
+        console.log(err.message)
+      }
+    }
+
+    getData()
+  }, [pageNo, tempCounter, authCtx?.user?.token])
+
   // collect bills and updates list
   useEffect(() => {
     handleFetchBills()
   }, [pageNo, tempCounter, authCtx?.user?.token])
 
   const handleViewBill = (id: any) => {
-    const filteredBill = billList.find((bill: any) => bill._id === id)
+    const filteredBill = newData.find((bill: any) => bill._id === id)
 
     if (filteredBill) {
       setSelectedBill(filteredBill)
@@ -127,12 +168,121 @@ const Bills = () => {
 
     tempCounter += 1
   }
-  console.log(billList)
+
+  const columns: GridColDef[] = [
+    {
+      field: "ranodm_1", // confirm this
+      headerName: "S.No",
+      valueGetter: (params) =>
+        params.api.getRowIndexRelativeToVisibleRows(params.id) + 1,
+    },
+    { field: "diaryNumber", headerName: "Diary No." },
+    { field: "name", headerName: "Name" },
+    { field: "billType", headerName: "Type" },
+    { field: "totalClaimedAmount", headerName: "Claimed Amount" },
+    {
+      field: "totalAdmissibleAmount",
+      headerName: "Admissible Amount",
+    },
+    { field: "sanctionedAmount", headerName: "Sanctioned Amount" },
+    { field: "currentStatus", headerName: "Status" },
+    {
+      field: "updatedAt",
+      headerName: "Last Updated",
+
+      valueFormatter: (params) => {
+        return dayjs(params.value).format("YYYY-MM-DD")
+      },
+    },
+    {
+      field: "random_2",
+      headerName: "Action",
+
+      renderCell: (params) => {
+        if (params.row.pendingBranch) {
+          return (
+            <Link
+              style={{
+                color: "#4C7AFF",
+                textDecoration: "none",
+              }}
+              href={`/Bills/ManageBill?bill_id=${params.row._id}&mode=${BILL_MODES.update}`}
+            >
+              Update
+            </Link>
+          )
+        } else {
+          return (
+            <button
+              style={{
+                background: "none",
+                border: "none",
+                color: "#4C7AFF",
+                fontSize: "13px",
+                padding: 0,
+                cursor: "pointer",
+              }}
+              onClick={() => handleViewBill(params.row._id)}
+            >
+              View
+            </button>
+          )
+        }
+      },
+    },
+  ]
+
   return (
     <PageContainer title="Bills" description="List of all the bills">
       <DashboardNew title="Bills" titleVariant="h5">
         <>
-          <Box>
+          <DataGrid
+            rows={newData}
+            columns={columns}
+            sx={{
+              ".bg-light": {
+                bgcolor: "#eee",
+                // "&:hover": {
+                //   bgcolor: "darkgrey",
+                // },
+              },
+              ".bg-dark": {
+                bgcolor: "#fff",
+              },
+              ".text-green": {
+                color: "green",
+              },
+              ".text-red": {
+                color: "red",
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: "#4C7AFF",
+                color: "#ffffff",
+                // fontWeight: "600",
+                // fontSize: "16px",
+              },
+            }}
+            getRowClassName={(params) => {
+              return (params.indexRelativeToCurrentPage + 1) % 2 === 0
+                ? "bg-light"
+                : "bg-dark"
+            }}
+            getCellClassName={(params) => {
+              if (params.field === "currentStatus") {
+                return params.row.currentStatus === "Open"
+                  ? "text-green"
+                  : "text-red"
+              }
+
+              return ""
+            }}
+            slots={{ toolbar: GridToolbar }}
+            initialState={{
+              pagination: { paginationModel: { pageSize: 10 } },
+            }}
+            pageSizeOptions={[10, 25, 50, 100]}
+          />
+          {/* <Box>
             <OptionsWrapper>
               <Button
                 sx={{ marginTop: "-23px" }}
@@ -276,7 +426,7 @@ const Bills = () => {
             url={`/api/claim/getall?name=${filterInputs.name}&diaryNumber=${filterInputs.diaryNumber}`}
             data={billList}
             setPageNo={setPageNo}
-          />
+          /> */}
 
           {/* BILL MODAL */}
           <CustomModal modalState={modalState} setModalState={setModalState}>
