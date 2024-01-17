b47b52a4289f0ba325d1c5ef2ab8958af2f48adf
Shrey Sharma <shrey.sharma@cipl.org.in>
2024-01-17 11:27:07 +0530

    Refactor: bill-routing page code clean up

--- src/app/(DashboardLayout)/BillRouting/page.tsx
+++ src/app/(DashboardLayout)/BillRouting/page.tsx
@@ -11,9 +11,9 @@ import MenuItem from "@mui/material/MenuItem"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 import { styled } from "@mui/system"
-import CloseIcon from '@mui/icons-material/Close';
-import AddIcon from '@mui/icons-material/Add';
-import SouthIcon from '@mui/icons-material/South';
+import CloseIcon from "@mui/icons-material/Close"
+import AddIcon from "@mui/icons-material/Add"
+import SouthIcon from "@mui/icons-material/South"
 import PageContainer from "../components/container/PageContainer"
 import DashboardNew from "../components/shared/DashboardNew"
 import { useAuth } from "@/context/JWTContext/AuthContext.provider"
@@ -29,13 +29,7 @@ const billType = [
   "Resident Telephone/Mobile charges Reimbursement",
 ]
 
-const dummyApiResponse = [
-  "Asst. Section Officer Admin I",
-  "Asst. Section Officer Admin IV",
-  "Section Officer Admin I",
-  "Section Officer Admin IV",
-]
-const selectOptions= [
+const selectOptions = [
   "Asst. Section Officer Admin I",
   "Asst. Section Officer Admin IV",
   "Section Officer Admin I",
@@ -52,19 +46,15 @@ const selectOptions= [
   "Accounts II",
   "Accounts IV",
   "F&BO",
-  "System Admin",
+  "System Admin", // remove this for only bill routing page
   "PAO",
   "Forwarded To Bank",
 ]
-// To show list of available options within the select field
-let sequenceOptions: any
 
 const BillRouting = () => {
-  const [selectedItems, setSelectedItems] = useState(['']); // Initial state with an empty item
-
   const [selectedBillType, setSelectedBillType] = useState("")
   const [billSequence, setBillSequence] = useState<any>([])
-  const [id, set_id] = useState<any>("")
+
   const authCtx: any = useAuth()
 
   useEffect(() => {
@@ -80,12 +70,9 @@ const BillRouting = () => {
 
       try {
         const res = await axiosApi(config.url, config.method, config.headers)
-        //   console.log(res)
+
         if (res && res.data) {
-          // console.log(res.data[0],"plplplllp");
-          set_id(res.data[0]._id)
-          setBillSequence(res.data[0].sequence)
-          sequenceOptions = res.data[0]
+          setBillSequence(res.data[0])
         }
       } catch (err: any) {
         console.log(err.message)
@@ -98,37 +85,31 @@ const BillRouting = () => {
   }, [selectedBillType, authCtx?.user?.token])
 
   const handleSequence = (e: any, i: any) => {
-    const billSequenceCopy = [...billSequence]
+    const billSequenceCopy = [...billSequence.sequence]
 
     billSequenceCopy[i] = e.target.value
 
-    setBillSequence((prevState: any) => ([
-      ...billSequenceCopy
-    ]))
-
-
+    setBillSequence((prevState: any) => ({
+      ...prevState,
+      sequence: billSequenceCopy,
+    }))
   }
 
   const handleFormSubmit = async (e: any) => {
     e.preventDefault()
 
-    console.log("abcd:", billSequence)
-
     const config = {
-      url: `/api/billRouting/update/${id}`,
+      url: `/api/billRouting/update/${billSequence._id}`,
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         authorization: `Bearer ${authCtx.user.token}`,
       },
       data: {
-        sequence: billSequence
+        sequence: [...billSequence.sequence],
       },
-      
     }
-    
-   
-     
+
     try {
       const res = await axiosApi(
         config.url,
@@ -136,74 +117,43 @@ const BillRouting = () => {
         config.headers,
         config.data
       )
-      
-      console.log(config.data,"config.data")
+
+      enqueueSnackbar(res.message, {
+        preventDuplicate: true,
+        variant: "success",
+      })
     } catch (err) {
       console.log(err)
     }
   }
-  //   const newitem =(props:any)=>{
 
-  // return (    <>
-
-  // <Select
-  //             size="small"
-  //             value={selectedBillType}
-  //             onChange={(e: any) => setSelectedBillType(e?.target?.value)}
-  //             sx={{ width: "100%" }}
-  //           >
-  //             {billType?.map((item, i) => (
-  //               <MenuItem key={i} value={item}>
-  //                 {item}
-  //               </MenuItem>
-  //             ))}
-  //           </Select>
-  //           <Button variant="contained" color="primary" sx={{
-
-  //                     height:"30px"
-  //                   }} 
-
-  //                   onClick={() => {
-  //                     addItemAtIndex(props.array, newitem,props.i)
-
-  //                   }}>
-  //                     <AddIcon/> Add
-  //                   </Button>
-  //                   <Button variant="contained" sx={{
-  //                     backgroundColor :"red",
-  //                     height:"30px"
-  //                   }}
-  //                   >
-  //                   <CloseIcon/>  Remove
-  //                   </Button>
-  //           </>)
-  //   }
-  console.log(billSequence, "newitem");
   function addItemAtIndex(array: any, item: any, index: any) {
     // Create a new array with the item inserted at the specified index
-    const newArray = [...array.slice(0, index), item, ...array.slice(index)];
+    const newArray = [...array.slice(0, index), item, ...array.slice(index)]
 
-    setBillSequence([...newArray]);
-    // return newArray;
-    console.log(billSequence);
+    setBillSequence((prevState: any) => ({ ...prevState, sequence: newArray }))
   }
+
   function removeItemAtIndex(array: any, index: any) {
     // Create a new array without the item at the specified index
-    const newArray = [...array.slice(0, index), ...array.slice(index + 1)];
-    setBillSequence(newArray);
+    const newArray = [...array.slice(0, index), ...array.slice(index + 1)]
+    setBillSequence((prevState: any) => ({ ...prevState, sequence: newArray }))
   }
+
   return (
     <PageContainer title="Bill Routing" description="Manage Former data here">
-      <DashboardNew title="Bill Routing" titleVariant="h5" >
-
-        <Box sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}>
-          <Box sx={{
-
-            width: "500px"
-          }}>
+      <DashboardNew title="Bill Routing" titleVariant="h5">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "500px",
+            }}
+          >
             <form>
               <InputLabel>Bill Type</InputLabel>
               <Select
@@ -219,90 +169,94 @@ const BillRouting = () => {
                   </MenuItem>
                 ))}
               </Select>
-              <Box sx={{
-                width: "90%",
-                mt:3,
-              }}>
-                {billSequence?.map((sequenceItem: any, i: any, array: any) => {
-                  return (
-                    <Box key={i}>
-                      <div>Role</div>
-                      <Box sx={{
-                        display: "flex",
-                        gap: "20px",
-                        alignItems: "center"
-                      }}>
-                        <Select
-                          // name={former.id}
-                          size="small"
-                          value={sequenceItem}
-                          //  onChange={(e: any) => setSelectedBillType(e?.target?.value)}
-                          onChange={(e: any) => handleSequence(e, i)}
-                          sx={{ width: "100%" }}
-                        >
-                          {selectOptions?.map(
-                            (item: any, innerIndex: any) => (
-                              <MenuItem key={innerIndex} value={item}>
-                                {item}
-                              </MenuItem>
-                            )
-                          )}
-                        </Select>
-
-
-
-                        <Button variant="contained" color="primary" sx={{
-
-                          height: "30px"
-                        }}
-
-                          onClick={() => {
-                            addItemAtIndex(array, "", i + 1)
-
-                          }}>
-                          <AddIcon /> Add
-                        </Button>
-                        <Button variant="contained" sx={{
-                          backgroundColor: "red",
-                          height: "30px",
-                          px: 3
-                        }}
-                          onClick={() => {
-                            removeItemAtIndex(array, i)
+              <Box
+                sx={{
+                  width: "90%",
+                  mt: 3,
+                }}
+              >
+                {billSequence?.sequence?.map(
+                  (sequenceItem: any, i: any, array: any) => {
+                    //   console.log("bankai:", array)
+                    return (
+                      <Box key={i}>
+                        <div>Role</div>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            gap: "20px",
+                            alignItems: "center",
                           }}
                         >
-                          <CloseIcon />  Remove
-                        </Button>
+                          <Select
+                            // name={former.id}
+                            size="small"
+                            value={sequenceItem}
+                            //  onChange={(e: any) => setSelectedBillType(e?.target?.value)}
+                            onChange={(e: any) => handleSequence(e, i)}
+                            sx={{ width: "100%" }}
+                          >
+                            {selectOptions?.map((item: any, i: any) => (
+                              <MenuItem key={i} value={item}>
+                                {item}
+                              </MenuItem>
+                            ))}
+                          </Select>
 
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                              height: "30px",
+                            }}
+                            onClick={() => {
+                              addItemAtIndex(array, "", i + 1)
+                            }}
+                          >
+                            <AddIcon /> Add
+                          </Button>
+                          <Button
+                            variant="contained"
+                            sx={{
+                              backgroundColor: "red",
+                              height: "30px",
+                              px: 3,
+                            }}
+                            onClick={() => {
+                              removeItemAtIndex(array, i)
+                            }}
+                          >
+                            <CloseIcon /> Remove
+                          </Button>
+                        </Box>
                       </Box>
-
-                    </Box>
-                  )
-                })}
+                    )
+                  }
+                )}
               </Box>
-              <Box sx={{
-                mt:3,
-                display:"flex",
-                justifyContent:"center"
-              }}>
-              <Box>
-            {
-                billSequence.length!=0?<Button variant="contained"
-                color="primary"
+              <Box
                 sx={{
-                  
-                  height: "30px",
-                  px: 3
+                  mt: 3,
+                  display: "flex",
+                  justifyContent: "center",
                 }}
-               onClick={handleFormSubmit}
               >
-                Submit 
-              </Button>:null
-              }
-            </Box>
+                <Box>
+                  {billSequence?.sequence?.length > 0 ? (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        height: "30px",
+                        px: 3,
+                      }}
+                      onClick={handleFormSubmit}
+                    >
+                      Submit
+                    </Button>
+                  ) : null}
+                </Box>
               </Box>
-            
-              
             </form>
           </Box>
         </Box>
