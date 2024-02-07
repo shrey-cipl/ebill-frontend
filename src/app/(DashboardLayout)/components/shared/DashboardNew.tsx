abb91eeaac613721fa1dcb68a2e9fd00e6db3c8e
RichieRich2020 <abhijeetkmbale9cd@gmail.com>
2024-02-07 17:15:21 +0530

    all push

--- src/app/(DashboardLayout)/components/shared/DashboardNew.tsx
+++ src/app/(DashboardLayout)/components/shared/DashboardNew.tsx
@@ -1,7 +1,10 @@
-import React ,{useContext}from "react"
+import React, { useContext } from "react"
 import { Card, CardContent, Typography, Stack, Box } from "@mui/material"
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
-import { CosmeticContext, useCosmetic } from "@/context/CosmeticContext/UseCosmetic.Provider"
+import {
+  CosmeticContext,
+  useCosmetic,
+} from "@/context/CosmeticContext/UseCosmetic.Provider"
 import { CircularProgress } from "@mui/material"
 // import { styled } from '@mui/system';
 type Props = {
@@ -35,131 +38,128 @@ const DashboardNew = ({
 
   let stitle = title?.slice(1)
 
- 
-  const cosmeticContext = useContext(CosmeticContext);
-  const { modalLoading, setModalLoading } = cosmeticContext;
+  const cosmeticContext = useContext(CosmeticContext)
+  const { modalLoading, setModalLoading } = cosmeticContext
 
-
+  //puah
   return (
-    
     <Card sx={{ padding: 0 }} elevation={9} variant={undefined}>
-     
       {cardheading ? (
         <>
-        <CardContent>
-          
-          <Typography variant="h5">{headtitle}</Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-            {headsubtitle}
-          </Typography>
-        </CardContent>
+          <CardContent>
+            <Typography variant="h5">{headtitle}</Typography>
+            <Typography variant="subtitle2" color="textSecondary">
+              {headsubtitle}
+            </Typography>
+          </CardContent>
         </>
       ) : (
         <>
-     
-        <CardContent sx={{ p: "10px" }}>
-          {title ? (
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              // alignItems={"center"}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent:"space-between",
-                  alignItems: "center",
-                  mb: 2,
-                }}
+          <CardContent sx={{ p: "10px" }}>
+            {title ? (
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                // alignItems={"center"}
               >
-                {stitle === "Dashboard" ? null : (
-                  <Box
-                    sx={{
-                      width: "80px",
-                      height: "35px",
-                      borderRadius: "5px",
-                      border: " 1px solid #fafafce8",
-                      backgroundColor: "#5d87ff",
-                      display: "flex",
-                      justifyContent: "center",
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    mb: 2,
+                  }}
+                >
+                  {stitle === "Dashboard" ? null : (
+                    <Box
+                      sx={{
+                        width: "80px",
+                        height: "35px",
+                        borderRadius: "5px",
+                        border: " 1px solid #fafafce8",
+                        backgroundColor: "#5d87ff",
+                        display: "flex",
+                        justifyContent: "center",
 
-                      alignItems: "center",
-                      mr: 2,
-                      fontSize: "14px",
-                      fontWeight: "bold",
-                      color: "white",
-                      cursor: "pointer",
-                    }}
-                    onClick={goBack}
-                  >
-                    <ArrowBackIosIcon
-                      sx={{
+                        alignItems: "center",
+                        mr: 2,
+                        fontSize: "14px",
+                        fontWeight: "bold",
                         color: "white",
-                        mr: -2,
-                        height: "15px",
+                        cursor: "pointer",
                       }}
-                    />
-                    <ArrowBackIosIcon
-                      sx={{
-                        color: "white",
-                        mr: -1,
-                        height: "15px",
-                      }}
-                    />
-                    Back
+                      onClick={goBack}
+                    >
+                      <ArrowBackIosIcon
+                        sx={{
+                          color: "white",
+                          mr: -2,
+                          height: "15px",
+                        }}
+                      />
+                      <ArrowBackIosIcon
+                        sx={{
+                          color: "white",
+                          mr: -1,
+                          height: "15px",
+                        }}
+                      />
+                      Back
+                    </Box>
+                  )}
+
+                  <Box>
+                    {" "}
+                    {title ? (
+                      <Typography variant={titleVariant ? titleVariant : "h5"}>
+                        {title}
+                      </Typography>
+                    ) : (
+                      ""
+                    )}
+                    {subtitle ? (
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {subtitle}
+                      </Typography>
+                    ) : (
+                      ""
+                    )}
                   </Box>
-                )}
-
-                <Box>
-                  {" "}
-                  {title ? (
-                    <Typography variant={titleVariant ? titleVariant : "h5"} >
-                      {title}
-                    </Typography>
-                  ) : (
-                    ""
-                  )}
-                  {subtitle ? (
-                    <Typography variant="subtitle2" color="textSecondary">
-                      {subtitle}
-                    </Typography>
-                  ) : (
-                    ""
-                  )}
-                 
                 </Box>
-                
-              </Box>
-              <Box>
-                  <Typography sx={{
-                    fontSize:"20px",
-                    color:"#5d87ff",
-                  }}>
-             { modalLoading? <> Loading..... <CircularProgress  size={30}/></>:null}
-                  </Typography></Box>
-                
-              {action}
-              
-            </Stack>
-          ) : null}
- <Box
- sx={{
-  position: 'relative',
-  filter: modalLoading?'blur(5px)':'blur(0px)', 
- }}
- >
-  <>{children}</>
-          
-          </Box>
-        </CardContent>
+                <Box>
+                  <Typography
+                    sx={{
+                      fontSize: "20px",
+                      color: "#5d87ff",
+                    }}
+                  >
+                    {modalLoading ? (
+                      <>
+                        {" "}
+                        Loading..... <CircularProgress size={30} />
+                      </>
+                    ) : null}
+                  </Typography>
+                </Box>
+
+                {action}
+              </Stack>
+            ) : null}
+            <Box
+              sx={{
+                position: "relative",
+                filter: modalLoading ? "blur(5px)" : "blur(0px)",
+              }}
+            >
+              <>{children}</>
+            </Box>
+          </CardContent>
         </>
       )}
 
       {middlecontent}
       {footer}
     </Card>
-   
- 
   )
 }
 
